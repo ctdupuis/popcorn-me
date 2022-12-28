555b19b2903f2ed2932a7cd21af78fedc8f2447b
Cody Dupuis <relationalprogrammer@gmail.com>
2022-12-28 13:04:48 -0600

    more scaffolding

--- src/components/static/Header.tsx
+++ src/components/static/Header.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import "../../styles/Header.css"
 
 export default function Header() {
   return (
-    <header>
-        Header
+    <header className='banner'>
+        <div className='logo'>
+          <Link to="/">
+            Popcorn & Me
+          </Link>
+        </div>
+        <div className='nav'>
+          <div className="nav-item">
+            <Link to="/movies">All movies</Link>
+          </div>
+          <div className="nav-item">
+            <Link to="/movies">All movies</Link>
+          </div>
+          <div className="nav-item">
+            <Link to="/movies">All movies</Link>
+          </div>
+        </div>
     </header>
   )
 }
--- src/components/static/Layout.tsx
+++ src/components/static/Layout.tsx
@@ -3,6 +3,8 @@ import { Outlet } from 'react-router-dom'
 import Footer from './Footer'
 import Header from './Header'
 
+// import "../../styles/Layout.css"
+
 export default function Layout() {
   return (
     <>
